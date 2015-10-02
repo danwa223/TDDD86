c5ff45ea4a91d1cc83f5931159a741d54b15f72f
danwa223 <d.wassing@gmail.com>
2015-10-02 13:20:01 +0200

    merge

--- lab3/tsp/src/tsp.cpp
+++ lab3/tsp/src/tsp.cpp
@@ -53,7 +53,7 @@ int main(int argc, char *argv[]) {
     double y;
     while (input >> x >> y) {
 		Point p(x, y);
-        tour.insertNearest(p);
+        tour.insertSmallest(p);
         //uncomment the 4 lines below to animate
         //tour.draw(scene);
         //std::chrono::milliseconds dura(50);
