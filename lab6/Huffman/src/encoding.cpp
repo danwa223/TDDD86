f493421cdfa46e76f5df4e0489f5829f17e846d2
Erik <e.sandberg92@gmail.com>
2015-11-23 13:44:19 +0100

    encodingTree = buildEncodingTree in huffmanmain.cpp is not returning properly.

--- lab6/Huffman/src/encoding.cpp
+++ lab6/Huffman/src/encoding.cpp
@@ -28,17 +28,38 @@ map<int, int> buildFrequencyTable(istream& input) {
 }
 
 HuffmanNode* buildEncodingTree(const map<int, int> &freqTable) {
+	cout << "asd";
 
-	map<int, int>::iterator it;
+	//map<int, int>::iterator it;
 	priority_queue<HuffmanNode> prioQueue;
 
-	for (it = freqTable.begin(); it != freqTable.end(); ++it) {
+	// Use the frequency table to build a priority queue of tree nodes
+	//map<int,int>::iterator and map<int, int>::const_iterator TODO: Ask about this
+	for (auto it = freqTable.begin(); it != freqTable.end(); ++it) {
 		prioQueue.push(HuffmanNode(it->first, it->second));
 	}
 
-	// build tree
+	HuffmanNode firstNode;
+	HuffmanNode secondNode;
 
-    return nullptr;
+	//
+	while(!(prioQueue.size() == 1)) {
+		firstNode = prioQueue.top();
+		prioQueue.pop();
+		secondNode = prioQueue.top();
+		prioQueue.pop();
+
+		cout << "Whileing...";
+		// Create a new node
+		// Count is the combined frequency of the two nodes, firstNode is the left and secondNode the right child
+		HuffmanNode newNode = HuffmanNode(NOT_A_CHAR, (firstNode.count + secondNode.count), &firstNode, &secondNode);
+
+		prioQueue.push(newNode);
+	}
+
+	// The priority queue should contain a tree by now
+	firstNode = prioQueue.top();
+	return firstNode;
 }
 
 map<int, string> buildEncodingMap(HuffmanNode* encodingTree) {
